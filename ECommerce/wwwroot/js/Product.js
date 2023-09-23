c1e42d4c3cb2e57e0c8a9e7609411216078d4a6d
TahooraNousheen <96386137+TahooraNousheen@users.noreply.github.com>
2023-09-23 20:17:07 +0530

    done

---
+++ ECommerce/wwwroot/js/Product.js
@@ -0,0 +1,184 @@
+﻿const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+    menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+    menu.style.visibility = "visible";
+});
+var app = angular.module('Product', []);
+
+
+
+app.controller('ProductController', function ProductController($scope, $rootScope, $http, $document) {
+    $scope.productItem = [{
+        itemid: 18,
+        productName: "Women Clothes",
+        price: "1800",
+        photo: "Women1.jpg",
+        info: "Wedding Wear Designer Silk Saree."
+
+    },
+    {
+        itemid:1,
+        productName: "Women Clothes",
+        price: "800",
+        photo: "Women2.jpeg"
+    },
+
+        {
+            itemid: 2,
+        productName: "Women Clothes",
+        price: "1200",
+        photo: "women3.jpg",
+        info: "Wedding Wear Designer Silk Saree."
+
+    },
+    {
+        itemid: 3,
+        productName: "Women Clothes",
+        price: "2200",
+        photo: "women4.jpg",
+        info: "Wedding Wear Designer Silk Saree."
+
+    },
+    {
+        itemid: 4,
+        productName: "Women Clothes",
+        price: "3000",
+        photo: "women8.jpg",
+        info: "Wedding Wear Designer Silk Saree."
+
+    },
+    {
+        itemid: 5,
+        productName: "Women Clothes",
+        price: "2500",
+        photo: "women9.jpeg",
+        info: "Wedding Wear Designer Silk Saree."
+    },
+
+    //Mens CLothes
+    {
+        itemid: 6,
+        productName: "Mens CLothes",
+        price: "1000",
+        photo: "Boy1.jpeg",
+        info: "Boys Plain Jeans Fasiionable"
+
+    },
+    {
+        itemid: 7,
+        productName: "Mens CLothes",
+        price: "1200",
+        photo: "boy2.jpeg",
+        info: "Boys Plain Jeans Fasiionable"
+
+    },
+    {
+        itemid: 8,
+        productName: "Mens CLothes",
+        price: "1500",
+        photo: "boy3.jpg",
+        info: "Boys Plain Jeans Fasiionable"
+
+    },
+    {
+        itemid: 9,
+        productName: "Mens CLothes",
+        price: "2500",
+        photo: "boy4.jpg",
+        info: "Boys Plain Jeans Fasiionable"
+
+    },
+    {
+        itemid: 10,
+        productName: "Mens CLothes",
+        price: "2200",
+        photo: "boy5.jpeg",
+        info: "Boys Plain Jeans Fasiionable"
+
+    },
+    {
+        itemid: 11,
+        productName: "Mens CLothes",
+        price: "2600",
+        photo: "boy6.jpeg",
+        info: "Boys Plain Jeans Fasiionable"
+
+    },
+
+    //Kids CLothes
+    {
+        itemid: 12,
+        productName: "Kids CLothes",
+        price: "500",
+        photo: "Boy1.jpeg",
+        info:"Boys Formal Suits. Classic Fit Blazer"
+    },
+    {
+        itemid: 13,
+        productName: "Kids CLothes",
+        price: "600",
+        photo: "boy2.jpeg",
+        info: "Boys Formal Suits. Classic Fit Blazer"
+    },
+    {
+        itemid: 14,
+        productName: "Kids CLothes",
+        price: "800",
+        photo: "boy3.jpg",
+        info: "Boys Formal Suits. Classic Fit Blazer"
+    },
+    {
+        itemid: 15,
+        productName: "Kids CLothes",
+        price: "1200",
+        photo: "boy4.jpg",
+        info: "Boys Formal Suits. Classic Fit Blazer"
+    },
+    {
+        itemid: 16,
+        productName: "Kids CLothes",
+        price: "500",
+        photo: "boy5.jpeg",
+        info: "Boys Formal Suits. Classic Fit Blazer"
+    },
+    {
+        itemid: 17,
+        productName: "Kids CLothes",
+        price: "1000",
+        photo: "boy6.jpeg",
+        info: "Boys Formal Suits. Classic Fit Blazer"
+    }
+
+    ];
+    $scope.addeditems = [];
+
+
+    $scope.addtocart = function (itemid,name, price) {
+        var checkitem = ($scope.addeditems.find(x => x.itemid == itemid));
+        if (checkitem == null) {
+            var item = { itemid: itemid, name: name, price: price, quantity: 1, totalamount: price };
+
+        }
+        else {
+
+        }
+
+        var item = { itemid: itemid, name: name, price: price, quantity: 1, totalamount: price };
+        $scope.addeditems.push(item);
+        //var totalamount = $scope.addeditems.reduce((n, { totalamount }) => n + totalamount, 0)
+        //alert(totalamount);
+    };
+
+    $scope.removeitem = function (itemid) {
+        $scope.addeditems.splice($scope.addeditems.indexOf($scope.addeditems.find(id => id.itemid == itemid)), 1);
+    }
+
+ 
+
+});
+
+
---
+++ Readme.md
@@ -0,0 +1,69 @@
+﻿**Sustainable Fashion E-Commerce Platform**
+
+Welcome to the README file for the Sustainable Fashion E-Commerce Platform. This platform is designed for a sustainable fashion brand, offering a dynamic shopping experience with a focus on environmental responsibility. The platform implements a dynamic shopping cart that displays the total price and allows users to adjust quantities. The application is intended to be run using Visual Studio 2019 and requires certain dependencies, frameworks, CSS files, images, and JavaScript files.
+
+**Getting Started**
+
+To run the single-page application, follow these steps:
+
+1. Open the solution file (.sln) in Visual Studio 2019.
+1. Build the solution to restore dependencies and compile the project.
+1. Run the application using Visual Studio's built-in web server or your preferred hosting method.
+
+**Dependencies**
+
+Make sure you have the following dependencies installed:
+
+- Microsoft.AspNetCore Analyzers
+- SDKAnalyzers
+- MVCAnalyzers
+
+**Frameworks**
+
+The project utilizes the following framework:
+
+- Microsoft.AspNetCore.App
+
+**CSS Files**
+
+The following CSS files are included:
+
+- **Product.css**
+- **site.css**
+
+**Images**
+
+The application includes 20 images related to the sustainable fashion brand.
+
+**JavaScript Files**
+
+The following JavaScript files are required:
+
+- **angular.js**
+- **Product.js**
+- **site.js**
+
+**Configuration**
+
+Configuration settings can be found in the **appsettings.json** file.
+
+**Project Structure**
+
+The project consists of the following files:
+
+- **Program.cs**: Entry point for the application.
+- **Startup.cs**: Configuration and middleware setup.
+- Other necessary files for the application's functionality.
+
+**Usage**
+
+Upon running the application, users can explore the sustainable fashion brand's products. The dynamic shopping cart allows users to adjust quantities and displays the total price.
+
+**Contributions**
+
+Contributions to the project are welcome. If you find any issues or would like to enhance the application, feel free to submit a pull request.-----
+
+Thank you for using the Sustainable Fashion E-Commerce Platform. 
+
+[208r1a12b2@gmail.com]
+
